64cc8fa1cc610d07b0f931e3502fcc724e8386fc
Xi-Plus <huangxuanyuxiplus@gmail.com>
2019-04-07 11:24:05 +0800

    Remove PasswordSecurity

--- config/default_setting.php
+++ config/default_setting.php
@@ -25,23 +25,13 @@ $C["CAPTCHAusestudent"] = false;
 $C["CAPTCHAsitekey"] = '';
 $C["CAPTCHAsecretkey"] = '';
 
-$C["PasswordSecurityEnabled"] = true;
-$C["PasswordSecurityMinLength"] = 4;
-$C["PasswordSecurityCannotBePopular"] = 100000;
-$C["PasswordSecurityPopularPasswordFile"] = 'path_to_your_file.txt';
-$G["PasswordSecurityText"] = [
-	"password_match_username" => "密碼與帳號相同",
-	"password_too_short" => "密碼太短，至少要" . $C["PasswordSecurityMinLength"] . "個字",
-	"password_is_popular" => "密碼在常見密碼列表中前" . $C["PasswordSecurityCannotBePopular"] . "位",
-];
-
 $C["FilenameReserved"] = '\/:*?"<>|';
 $C["FilenamePattern"] = '[^';
 foreach (str_split($C["FilenameReserved"]) as $char) {
-	$C["FilenamePattern"] .= '\x'.sprintf("%x", ord($char));
+	$C["FilenamePattern"] .= '\x' . sprintf("%x", ord($char));
 }
 $C["FilenamePattern"] .= '\x00-\x1f\x7f]+';
-$C["FilenameTitle"] = "不可包含控制字元和以下字元: ".htmlentities(implode(" ", str_split($C["FilenameReserved"])));
+$C["FilenameTitle"] = "不可包含控制字元和以下字元: " . htmlentities(implode(" ", str_split($C["FilenameReserved"])));
 
 date_default_timezone_set("Asia/Taipei");
 
