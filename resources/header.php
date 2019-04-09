38fd37bcf713f7cc02bc7677121649c41b5d5da4
Xi-Plus <huangxuanyuxiplus@gmail.com>
2019-04-09 17:54:31 +0800

    Rename manage_apply to manage_qualification

--- resources/header.php
+++ resources/header.php
@@ -27,7 +27,7 @@
                         <a class="dropdown-item" href="<?=$C["path"]?>/manage/data/new"><i class="fas fa-database"></i> 新增資料</a>
                         <a class="dropdown-item" href="<?=$C["path"]?>/manage/upload/"><i class="fa fa-user"
                                 aria-hidden="true"></i> 上傳檔案</a>
-                        <a class="dropdown-item" href="<?=$C["path"]?>/manage/apply/"><i class="fa fa-user"
+                        <a class="dropdown-item" href="<?=$C["path"]?>/manage/qualification/"><i class="fa fa-user"
                                 aria-hidden="true"></i> 申請資格</a>
                         <a class="dropdown-item" href="<?=$C["path"]?>/manage/account/"><i class="fa fa-user"
                                 aria-hidden="true"></i> 帳號</a>
